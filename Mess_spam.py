d140637c5da0bfd04293768495792ca0d08a9bcd
Karamu11 <128216260+Karamu11@users.noreply.github.com>
2023-10-15 17:48:06 +0700

    Update Mess_spam.py

--- Mess_spam.py
+++ Mess_spam.py
@@ -1,12 +1,6 @@
-from emoji.core import emoji_count
-import requests, os
-import socket
-from time import sleep
-import emoji
 import os
-
-os.system("")
-
+import socket
+from concurrent.futures import ThreadPoolExecutor
 class style():
     BLACK = '\033[30m'
     RED = '\033[31m'
@@ -26,15 +20,15 @@ class style():
     ENDC = '\033[0m'
     BOLD = "\033[1m"
     TAU = "\U0001f680"
-
+os.system("clear")
 ip=socket.gethostbyname(socket.gethostname())
 th='- - - - - - - - - - - - - - - - - - - - - - - - -'
-print(style.HEADER +emoji.emojize(':red_heart:  SPAM MESSENGER :red_heart:'))
+print(style.HEADER +('SPAM MESSENGER AOGAME :'))
 print(style.BLUE+'----------------------------')
-id=input(style.OKGREEN + '\U0001f4aa NHẬP ID MỤC TIÊU \U0001f64a :')
+id=input(style.OKGREEN + 'NHẬP ID MỤC TIÊU :')
 print(style.BLUE+'----------------------------')
 while True:
-  ck=input(style.GREEN + '\U0001f58a\uFE0F  NHẬP COOKIE FACEBOOK \U0001f4dc :')
+  ck=input(style.GREEN + 'NHẬP COOKIE FACEBOOK :')
   print(style.BLUE+'----------------------------')
   try:
         get=requests.get(f'https://mbasic.facebook.com/privacy/touch/block/confirm/?bid={id}&ret_cancel&source=profile',headers={'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9','accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5','cookie': ck,'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"','sec-ch-ua-mobile': '?0','sec-ch-ua-platform': '"Windows"','sec-fetch-dest': 'document','sec-fetch-mode': 'navigate','sec-fetch-site': 'same-origin','sec-fetch-user': '?1','upgrade-insecure-requests': '1'}).text
@@ -42,15 +36,15 @@ while True:
         jazoest=get.split('<input type="hidden" name="jazoest" value="')[1].split('" autocomplete="off" />')[0]
         break
   except:
-        print(style.RED +'SAI COOKIE RỒI NGU QUÁ \U0001f602')
+        print(style.RED +'SAI COOKIE RỒI NGU QUÁ ')
 
 
 
-nd=input(style.GREEN + '\u270D\uFE0F Nhập nội dung : ')
+nd=input(style.GREEN + 'Nhập nội dung : ')
 print(style.BLUE+'----------------------------')
-so_luong=int(input(style.GREEN +'\U0001f525Nhập số lần spam : '))
+so_luong=int(input(style.GREEN +'Nhập số lần spam : '))
 print(style.BLUE+'----------------------------')
-delay=int(input('\u23F1\uFE0F Nhập delay: '))
+delay=int(input('Nhập delay: '))
 print(style.BLUE+'----------------------------')
 headers = {
     'authority': 'm.facebook.com',
@@ -84,7 +78,7 @@ data = {
 }
 for i in range(1,so_luong+1):
     response = requests.post('https://m.facebook.com/messages/send/', params=params, headers=headers, data=data)
-    print(style.GREEN + f'{i}. \u2694\uFE0F  Mục Tiêu Đã Bị Tấn Công \U0001f680 | {nd} \U0001f3f9')
+    print(style.GREEN + f'Mục Tiêu Đã Bị Tấn Công ')
     sleep(delay)
 print(style.BLUE+'----------------------------')
 print(style.HEADER +'__[KẾT THÚC TẤN CÔNG]__')
