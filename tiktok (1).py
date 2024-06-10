c0a75be7f1a2cd572217bb2695de255896d2f208
Karamu11 <128216260+Karamu11@users.noreply.github.com>
2024-06-11 00:32:44 +0700

    Add files via upload

---
+++ tiktok (1).py
@@ -0,0 +1,30 @@
+print('Xoá Tất Cả Các Tệp')
+def my_funcion() :
+    print('* Variable 1')
+    a=b=c=d="Đã Xoá Thành Công"
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(" ")
+    print('* Variable 2')
+    a=b=c=d="Đã Xoá Thành Công"
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(" ")
+    print('* Variable 3')
+    a=b=c=d="Đã Xoá Thành Công"
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(" ")
+    print('* Variable 4')
+    a=b=c=d="Đã Xoá Thành Công"
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+my_funcion()
