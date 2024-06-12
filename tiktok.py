a643b06be94600c042e45f2b8005800fc4cfff79
Karamu11 <128216260+Karamu11@users.noreply.github.com>
2024-06-12 15:40:54 +0700

    Update tiktok.py

--- tiktok.py
+++ tiktok.py
@@ -1,4 +1,58 @@
 import os
+import os
+import requests,os,time,re,json,uuid,random,sys
+from concurrent.futures import ThreadPoolExecutor
+import requests
+from datetime import datetime
+import socket
+import random
+import time
+import colorama
+from colorama import Fore, Style
+from threading import Thread
+from bs4 import BeautifulSoup
+from socket import socket, AF_INET, SOCK_DGRAM
+import os
+from threading import Thread
+from random import choices, randint
+import requests
+from pystyle import *
+import socket
+from getpass import getpass as hinput
+from datetime import datetime
+now = datetime.now()
+import datetime
+import os
+import requests,os,time,re,json,uuid,random,sys
+from concurrent.futures import ThreadPoolExecutor
+from datetime import date
+from time import sleep,strftime
+from pystyle import *
+from time import sleep 
+import requests, random
+import requests
+import base64, json,os
+from time import sleep,strftime
+import re,requests,os,sys
+from time import sleep 
+import requests, random
+import uuid, re
+from pystyle import Write,Colors
+import socket
+from datetime import datetime
+def get_ip_from_url(url):
+    response = requests.get(url)
+    ip_address = socket.gethostbyname(response.text.strip())
+    return ip_address
+
+
+try:
+    url = "http://kiemtraip.com/raw.php"
+    ip_thaycham = get_ip_from_url(url).replace('.', '')
+    ip1 = int(ip_thaycham)
+except:
+    print("Mạng lỏ đcmm bật mạng lên cho tao nhanh lên :))", end='\n')
+    exit()
 os.system('clear')
 print('\033[1;34mXoá Tất Cả Các Tệp')
 def my_funcion() :
